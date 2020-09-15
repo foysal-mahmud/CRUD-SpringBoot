aa42936c4c69356feb25ed7bb1b07c75a9ca9f46
Md. Foysal Mahmud <59803500+foysal-mahmud@users.noreply.github.com>
2020-09-15 20:41:34 +0600

    Add files via upload

---
+++ src/main/java/com/foysal/springBootCRUD/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.foysal.springBootCRUD.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.foysal.springBootCRUD.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
